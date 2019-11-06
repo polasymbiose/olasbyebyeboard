e5c66149d824654bb1cf6b4efcf213bc82ea9561
Paul Mura <p.mura@Pauls-MacBook-Pro.local>
2019-11-06 23:13:05 +0100

    build

---
+++ build/precache-manifest.28cb68e0bc433a0e30b69d979b29fa7e.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d8bdc83f69f7aed19e4d39eaa509e9e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65be22db2876b26de825",
+    "url": "/static/css/main.46b67600.chunk.css"
+  },
+  {
+    "revision": "8c112b4b0400d2ca6587",
+    "url": "/static/js/2.3bb0adda.chunk.js"
+  },
+  {
+    "revision": "65be22db2876b26de825",
+    "url": "/static/js/main.6f048644.chunk.js"
+  },
+  {
+    "revision": "e439cb1e81fca27bef16",
+    "url": "/static/js/runtime-main.560b8e60.js"
+  },
+  {
+    "revision": "02583e297846b5eb5526d467004057ed",
+    "url": "/static/media/boo.02583e29.png"
+  },
+  {
+    "revision": "fa8147dc2fe115c730923adc8ec08f2e",
+    "url": "/static/media/boo.fa8147dc.mp3"
+  },
+  {
+    "revision": "89fb270401449bb9ddf70ed8972705ac",
+    "url": "/static/media/brainfuck.89fb2704.png"
+  },
+  {
+    "revision": "950f5c2880c041b082cc7ed543aea23f",
+    "url": "/static/media/brainfuck.950f5c28.mp3"
+  },
+  {
+    "revision": "ecb12f0d8cd96a481221f5c9c9da653f",
+    "url": "/static/media/lol.ecb12f0d.png"
+  },
+  {
+    "revision": "f04333bc49f705e2cd4d4868cc2f4757",
+    "url": "/static/media/lol.f04333bc.mp3"
+  },
+  {
+    "revision": "19b9e153705d413aadd4c221f4e5ebf0",
+    "url": "/static/media/love.19b9e153.mp3"
+  },
+  {
+    "revision": "953646177aa59357ca7c6c623eb2b533",
+    "url": "/static/media/love.95364617.png"
+  },
+  {
+    "revision": "69d2b71fcb5df388f6dff915af4bf81d",
+    "url": "/static/media/thinking.69d2b71f.png"
+  },
+  {
+    "revision": "6fa45fe5bfb464f884f56100008e2810",
+    "url": "/static/media/thinking.6fa45fe5.mp3"
+  },
+  {
+    "revision": "397167f9d10146f9d74614cf512c930e",
+    "url": "/static/media/wow.397167f9.png"
+  },
+  {
+    "revision": "7960067abd560593ce3c1d015cf7796e",
+    "url": "/static/media/wow.7960067a.mp3"
+  },
+  {
+    "revision": "01f0e794476c5fcb349fae45e9454f25",
+    "url": "/static/media/yes.01f0e794.mp3"
+  },
+  {
+    "revision": "d3ff40c1ca53153e77fdde961c70b721",
+    "url": "/static/media/yes.d3ff40c1.png"
+  }
+]);
